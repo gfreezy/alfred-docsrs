715953f08f141a32e2618f4b59c9606475210691
gfreezy <gfreezy@gmail.com>
2019-12-13 15:48:27 +0800

    debug2

--- release.sh
+++ release.sh
@@ -2,10 +2,12 @@
 pwd
 ls
 ls workflow
-if [ ! -f workflow/docsrs ]; then
+if [ ! -f "workflow/docsrs" ]; then
     cargo build --release
     cp target/release/docsrs workflow/
 fi
+
+chmod +x workflow/docsrs
 
 ./package.sh workflow .
 rm workflow/docsrs
