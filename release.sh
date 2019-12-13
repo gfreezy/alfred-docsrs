5de83e7e5cf9edb0625c934593238deb7daab272
gfreezy <gfreezy@gmail.com>
2019-12-13 16:52:39 +0800

    test debug

--- release.sh
+++ release.sh
@@ -1,9 +1,12 @@
 #!/bin/bash -e
+DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd $DIR
 ls -l workflow/docsrs
+test -f workflow/docsrs
 
-[ ! -f "workflow/docsrs" ] && echo not exist
+[ ! -f "$DIR/workflow/docsrs" ] && echo not exist
 
-if [ ! -f "workflow/docsrs" ]; then
+if [ ! -f "$DIR/workflow/docsrs" ]; then
     cargo build --release
     cp target/release/docsrs workflow/
 fi
